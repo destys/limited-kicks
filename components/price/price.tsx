73ebfc220cd80401dc225156a7aaa937c17c3f65
destys <desstyss@gmail.com>
2024-03-18 18:07:24 +0300

    catalog and seo progress

--- components/price/price.tsx
+++ components/price/price.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 interface PriceProps {
   before?: string;
-  value: string;
+  value: any;
   after?: string;
   className?: string;
 }
 
-const Price: React.FC<PriceProps> = ({ before, value, after, className }) => {;
+const Price: React.FC<PriceProps> = ({ before, value, after, className }) => {
+
+
   const price = parseFloat(value).toLocaleString("ru-RU", {
     style: "currency",
     currency: "RUB",

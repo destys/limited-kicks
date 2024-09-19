73b47b0ebd092705bd174e9a921f00a6f516a39c
destys <desstyss@gmail.com>
2024-09-19 15:39:53 +0300

    fix:search

--- actions/get-search-results.tsx
+++ actions/get-search-results.tsx
@@ -1,17 +1,26 @@
 import { fetchWooCommerce } from "@/lib/utils";
 import { Product, Products } from "@/types";
 
+// Запрос к кастомному эндпоинту для поиска по заголовку и SKU
+const fetchCustomSearchResults = async (search: string, per_page: number): Promise<Products[]> => {
+    const response = await fetch(`${process.env.WP_ADMIN_REST_URL}/custom-api/v1/search-products?search=${search}&per_page=${per_page}`);
+    if (!response.ok) {
+        throw new Error('Ошибка поиска товаров');
+    }
+    const data = await response.json();
+    return data;
+};
+
 interface IQuery {
     search: string;
     per_page: number;
 }
 
 const getSearchResults = async (query: IQuery): Promise<Products[]> => {
-    const res = await fetchWooCommerce('products', query);
+    // Выполняем запрос к кастомному API, который ищет как по заголовку, так и по SKU
+    const resByCustomApi = await fetchCustomSearchResults(query.search, query.per_page);
 
-    return res.filter((product: Product) =>
-        product.name.toLowerCase().includes(query.search.toLowerCase())
-    );
+    return resByCustomApi;
 };
 
 export default getSearchResults;
